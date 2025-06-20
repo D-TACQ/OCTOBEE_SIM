0c001dac4bdb62007b3f4b1d9fcaf9124fc04916
Craig Hay <craig.hay@d-tacq.co.uk>
2025-06-20 16:35:52 +0100

    Add way to get encoder ticks
    Encoder ticks now based on linear stage position and go back and
    forward as they should.
    
    Conversion from float millimetres to encoder ticks each sample
    count is plucked out of thin air. Multiplying by a factor every
    sample to get from tiny fractions of a mm (sampling at 10kHz) to
    integer numbers of encoder ticks each sample.
    
    But it will do for now.

--- write_muxed_data.py
+++ write_muxed_data.py
@@ -15,6 +15,10 @@ def calculate_n_samples(n_yscans, n_xscans=0, n_zscans=0, sample_rate=10000):
     YSCAN_TIME = SCAN_SETUP["durations_s"]["y_scan"]
     ZSCAN_TIME = SCAN_SETUP["durations_s"]["z_scan"]
 
+    # input validation
+    if (n_yscans < n_zscans) or (n_zscans < n_xscans):
+        raise ValueError("Check scan counts")
+
     n_samples = sample_rate * (
         XSCAN_TIME * n_xscans + YSCAN_TIME * n_yscans + ZSCAN_TIME * n_zscans
     )
@@ -30,7 +34,7 @@ def n_samples_to_elapsed_usec(sample_rate, n_samples):
 def find_optimum_gain(arr):
     """Finds optimum gain for array data by using maximum value of array
     and dynamic range of selected ADC"""
-    dynamic_range_bits = SAMPLING['dynamic_range_bits']
+    dynamic_range_bits = SAMPLING["dynamic_range_bits"]
     m = arr.max()
     n = arr.min()
     dynamic_range = 2**dynamic_range_bits
@@ -43,16 +47,24 @@ def find_optimum_gain(arr):
     else:
         return gain2
 
+def convert_mm_to_ticks(a):
+    # plucked this conversion factor out of thin air
+    return a * 10000
+
 if __name__ == "__main__":
     filename_list = FILES["input_list"]
+    scan_path_filename = FILES["recorded_scan_path"]
     N_SENSORS = SYSTEM_PARAMETERS["sensor_count"]
     N_XSCAN = SCAN_SETUP["scan_counts"]["x"]
     N_YSCAN = SCAN_SETUP["scan_counts"]["y"]
     N_ZSCAN = SCAN_SETUP["scan_counts"]["z"]
+    SAMPLE_RATE = SAMPLING["rate_hz"]
 
     generated_dtype = data_writer.n_sensors_dtype_generator(N_SENSORS)
     print(generated_dtype)
-    n_samples = calculate_n_samples(n_yscans=N_YSCAN)
+    n_samples = calculate_n_samples(
+        n_yscans=N_YSCAN, n_xscans=N_XSCAN, n_zscans=N_ZSCAN, sample_rate=SAMPLE_RATE
+    )
     # read in data
     result = np.zeros(n_samples, dtype=generated_dtype)
     print(f"shape of result is {result.shape} and dtype is {result.dtype}")
@@ -78,11 +90,18 @@ if __name__ == "__main__":
     elapsed_usec = n_samples_to_elapsed_usec(10000, n_samples)
 
     # TODO: populate positions based on encoder ticks
-    result["XPOS"] = np.zeros(n_samples)
-    result["YPOS"] = np.linspace(
-        0, 300, n_samples
-    )  # TODO: change this from hardcoded mm
-    result["ZPOS"] = np.zeros(n_samples)
+    scan_path = np.load(scan_path_filename)
+    scan_path_selection = scan_path[0:n_samples,:]
+    print(scan_path_selection.shape)
+    result["XPOS"] = convert_mm_to_ticks(scan_path_selection[:,0])
+    print(scan_path_selection[:,0])
+    print(scan_path_selection[:,0].shape)
+    result["YPOS"] = convert_mm_to_ticks(scan_path_selection[:,1])
+    print(scan_path_selection[:,1])
+    print(scan_path_selection[:,1].shape)
+    result["ZPOS"] = convert_mm_to_ticks(scan_path_selection[:,2])
+    print(scan_path_selection[:,2])
+    print(scan_path_selection[:,2].shape)
     result["CNT"] = np.linspace(0, n_samples, n_samples)
     result["USEC"] = np.linspace(0, elapsed_usec, n_samples)
 
